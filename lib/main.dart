eea9ddc2890f2a46a4a24474c739cd0c78ef304f
Ben-Apps <124107303+BenAppAkademie@users.noreply.github.com>
2025-11-18 21:14:16 +0100

    Add firestore and first data structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:b11_app/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'meal.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,8 +16,40 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    final firestore = FirebaseFirestore.instance;
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Meals")),
+        body: StreamBuilder(
+          stream: firestore.collection("meals").snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("Fehler: ${snapshot.error}");
+            }
+
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            final docs = snapshot.data!.docs;
+
+            final meals = docs.map((doc) {
+              return Meal.fromMap(doc.id, doc.data());
+            }).toList();
+
+            return ListView.builder(
+              itemCount: meals.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text(meals[i].name),
+                  subtitle: Text(meals[i].mealtype.name),
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/meal.dart
@@ -0,0 +1,31 @@
+enum MealType { VEGAN, VEGETARIAN, OMNIVORE }
+
+class Meal {
+  final String id;
+  final String name;
+  final MealType mealtype;
+
+  Meal({
+    required this.id,
+    required this.name,
+    required this.mealtype,
+  });
+
+  factory Meal.fromMap(String id, Map<String, dynamic> data) {
+    return Meal(
+      id: id,
+      name: data['name'] ?? '',
+      mealtype: MealType.values.firstWhere(
+        (e) => e.name == (data['mealtype'] ?? '').toString().toUpperCase(),
+        orElse: () => MealType.OMNIVORE,
+      ),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "name": name,
+      "mealtype": mealtype.name, // schönere Form
+    };
+  }
+}
